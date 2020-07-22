f79b20fd39d8b4c6a23daa12436515c510c11db2
SaiyemBejoy <jsbejoy96@gmail.com>
2020-07-22 23:45:25 +0600

    First commit after completting 50% of the project

---
+++ Inventory_Management_System/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory_Management_System.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult Index(user u)
+        {
+            inventoryDBEntities obj = new inventoryDBEntities();
+            var data = obj.st_getLoginDetails(u.u_username, u.u_password);
+            foreach(var item in data)
+            {
+                if(item.Username == u.u_username && item.Password == u.u_password)
+                {
+                    Session["name"] = u.u_username;
+                    return RedirectToAction("Main");
+                }
+                else
+                {
+
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Remove("name");
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Main()
+        {
+            if(Session["name"]== null)
+            {
+                return RedirectToAction("Index", "Auth");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Inventory_Management_System/Controllers/rolesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Inventory_Management_System;
+
+namespace Inventory_Management_System.Controllers
+{
+    public class rolesController : Controller
+    {
+        private inventoryDBEntities db = new inventoryDBEntities();
+
+        // GET: roles
+        public ActionResult Index()
+        {
+            return View(db.roles.ToList());
+        }
+
+        // GET: roles/Details/5
+        public ActionResult Details(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            role role = db.roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // GET: roles/Create
+        public ActionResult Create()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            list.Add(new SelectListItem() { Text = "Active", Value = "1" });
+            list.Add(new SelectListItem() { Text = "Inactive", Value = "0" });
+            ViewBag.abc = new SelectList(list, "Value", "Text");
+
+            return View();
+        }
+
+        // POST: roles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "r_id,r_name,r_status")] role role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.roles.Add(role);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(role);
+        }
+
+        // GET: roles/Edit/5
+        public ActionResult Edit(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            List<SelectListItem> list = new List<SelectListItem>();
+            list.Add(new SelectListItem() { Text = "Active", Value = "1" });
+            list.Add(new SelectListItem() { Text = "Inactive", Value = "0" });
+            ViewBag.abc = new SelectList(list, "Value", "Text");
+
+            role role = db.roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: roles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "r_id,r_name,r_status")] role role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(role).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(role);
+        }
+
+        // GET: roles/Delete/5
+        public ActionResult Delete(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            role role = db.roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(byte id)
+        {
+            role role = db.roles.Find(id);
+            db.roles.Remove(role);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
